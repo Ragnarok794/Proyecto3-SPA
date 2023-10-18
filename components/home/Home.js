b4b8aa05b1875c67e9f41a3cd297e0c590edf5c5
Ragnarok794 <adrianizq94@gmail.com>
2023-10-18 12:56:32 +0200

    footer fixed

--- components/home/Home.js
+++ components/home/Home.js
@@ -12,7 +12,7 @@ const photoPerfil =` <div>
 <div class="photo"><img src="./foto_perfil.jpg"></div></div>`
 
  const information = 
-`<div class="text">
+`<div class="text_home">
   <p class="title"><h3> Desarrollador Full Stack Innovador Listo para Impulsar tu Equipo</h3></p>
   <p class="introduction">Hola! Soy Adrian, un apasionado Desarrollador Full Stack con una sólida experiencia en la creación de soluciones web robustas y escalables. Mi enfoque va más allá del código; me encanta construir experiencias digitales excepcionales que transformen ideas en realidades digitales impresionantes.
 
